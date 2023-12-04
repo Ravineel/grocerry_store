22a1c788e5a130b214efa13d88b314f8cde1f062
Ravineel Singhi <rajatsinghi89@gmail.com>
2023-12-05 00:20:59 +0530

    tweaks

---
+++ Frontend/src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container-fluid">
+    <h1>Dashbaord</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+};
+</script>
+
+<style scoped></style>
