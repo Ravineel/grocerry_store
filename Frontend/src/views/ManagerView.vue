6a4dd034a4d377f4ca77be6563ed9803058d39d6
Ravineel Singhi <rajatsinghi89@gmail.com>
2025-01-06 05:24:45 +0530

    tweek

--- Frontend/src/views/ManagerView.vue
+++ Frontend/src/views/ManagerView.vue
@@ -75,9 +75,7 @@ export default {
     },
     async onClickDownlaodReport() {
       const res = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}${
-          API_ENDPOINTS.REPORT.GET_PRODUCT_REPORT
-        }`
+        `https://flaskbackend.sgccl.in:8443/api/v1${API_ENDPOINTS.REPORT.GET_PRODUCT_REPORT}`
       );
       const data = await res.json();
 
